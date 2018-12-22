674fcbb6fd76b749df100bb8a2c2e7edaecb8ee5
Cooper Runstein <cooper.runstein@vacasa.com>
2018-12-22 14:11:48 -0800

    initialImplementation - wired TestCafe for Integration testting

---
+++ IntegrationTests/app.js
@@ -0,0 +1,8 @@
+//test()
+import { Selector } from "testcafe";
+
+fixture`Getting Started`.page`http://devexpress.github.io/testcafe/example`;
+
+test("My first test", async t => {
+  await t.typeText("#developer-name", "John Smith").click("#submit-button");
+});
